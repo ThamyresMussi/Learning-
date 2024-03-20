859f90e32b0db944273c93602bf71500967e7bff
ThamyresMussi <160727709+ThamyresMussi@users.noreply.github.com>
2024-03-20 10:07:48 -0300

    Add files via upload

---
+++ Exemplo2.1.py
@@ -0,0 +1,165 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+2=
+SyntaxError: cannot assign to expression
+ 2+2
+ 
+SyntaxError: unexpected indent
+2+2
+4
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+nome
+'João'
+numero
+10
+numero * 2
+20
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+
+a
+10
+b
+5
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2.0
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é
+Traceback (most recent call last):
+  File "C:/Users/202103203817/Documents/Exercícios/Exempo1.py", line 27, in <module>
+    palavra= Direito
+NameError: name 'Direito' is not defined
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+Traceback (most recent call last):
+  File "C:/Users/202103203817/Documents/Exercícios/Exempo1.py", line 20, in <module>
+    print (" A média é",media)
+NameError: name 'media' is not defined. Did you mean: 'média'?
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de letras 7
+Traceback (most recent call last):
+  File "C:/Users/202103203817/Documents/Exercícios/Exempo1.py", line 40, in <module>
+    print ("a frase é",frases)
+NameError: name 'frases' is not defined. Did you mean: 'frase'?
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de letras 7
+a frase é Hoje eu vou estudar
+quantidade de espaços 3
+a palavra é Direito
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de letras 7
+a frase é Hoje eu vou estudar
+quantidade de espaços 3
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+a frase é Hoje eu vou estudar
+a frase é Hoje eu vou estudar len(palavra)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+"a frase é Hoje eu vou estudar" len(palavra)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+'a frase é Hoje eu vou estudar' len(palavra)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+a frase é Hoje eu vou estudar
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+a frase é Hoje eu vou estudar
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de espaços em branco da frase Hoje eu vou estudar
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de espaços em branco da frase Hoje eu vou estudar
+quantidade
+
+================================================== RESTART: C:/Users/202103203817/Documents/Exercícios/Exempo1.py =================================================
+THAMYRES
+ A média é 12.0
+ o quadrado é 16
+o dobro é 32
+o segundo número é maior que o primeiro True
+o maior número é 20
+a palavra é Direito
+quantidade de espaços em branco da frase Hoje eu vou estudar
+quantidade 3
