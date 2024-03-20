9b4e03ed6a6092d6103f1c3380785db4cc59689f
ThamyresMussi <160727709+ThamyresMussi@users.noreply.github.com>
2024-03-20 10:08:04 -0300

    Add files via upload

---
+++ Exempo1.py
@@ -0,0 +1,45 @@
+print ("THAMYRES")
+
+numero = 10
+nome = "João"
+booleano = True 
+
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplacação = a * b
+divisão = a / b 
+
+w = 4
+x = 8
+y = 16
+z = 20
+
+média= (w + x + y + z) / 4
+print (" A média é", média)
+
+quadrado= w * w
+print (" o quadrado é", quadrado)
+
+dobro=  y * 2
+print ("o dobro é", dobro)
+
+números= x>w
+print ("o segundo número é maior que o primeiro",números)
+
+maior= max(w,x,y,z)
+print ("o maior número é",maior)
+
+palavra= "Direito"
+len(palavra)
+print("a palavra é",palavra)
+
+
+
+frase= "Hoje eu vou estudar" 
+print ("quantidade de espaços em branco da frase",frase)
+frase.count(' ')
+print ("quantidade",frase.count(' '))
+
+
