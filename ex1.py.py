a9018eeaa1ba9ac456ae2893485ba0ea871c788a
ThamyresMussi <160727709+ThamyresMussi@users.noreply.github.com>
2024-04-01 22:28:16 -0300

    Add files via upload

---
+++ ex1.py.py
@@ -0,0 +1,27 @@
+def calcular_soma_e_media(lista):
+    soma = sum(lista)
+    media = soma/len(lista)
+    return soma, media
+
+def substituir_palavra(lista,palavra_procurada,nova_palavra):
+     nova_lista = [nSova_palavra if palavra = palavra_procurada else palavra for palavra in lista]
+     return nova_lista
+
+    def gerar_triangulo(num_linhas):
+        for in range(1,num_linhas + 1):
+            print('*'*i)
+
+    if_name_=="_main_":
+        lista_numeros = [18,26,8,68]
+        soma,media=
+    calcular_soma_e_media(lista_numeros)
+        print("Soma:",soma)
+        print("Média:",media)
+
+        lista_palavras = ["melancia", "morango", "abacaxi"]
+        nova_lista = substituir_palavra(lista_palavras,"melancia","maça")
+         print("lista alterada:",nova_lista)
+
+         num_linhas = 10
+         gerar_triangulo(num_linhas)
+    
